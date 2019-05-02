d8b3e81474838bc86bd05f3ca0eccc12007471ef
Xrysnow <xrysnow@outlook.com>
2019-05-02 22:02:35 +0800

    update native.lua

--- platform/android/native.lua
+++ platform/android/native.lua
@@ -4,7 +4,7 @@
 --- Copyright (C) 2018-2019 Xrysnow. All rights reserved.
 ---
 
-local native = {}
+local M = {}
 local luaj = require('cocos.cocos2d.luaj')
 local call = luaj.callStaticMethod
 local osname = ex.GetOSName()
@@ -23,14 +23,14 @@ local andHelper = 'org/cocos2dx/lib/Cocos2dxHelper'
 local andApp = 'org/cocos2dx/lua/AppActivity'
 
 ---@return string
-function native.getAssetsPath()
+function M.getAssetsPath()
     local methodName = 'getAssetsPath'
     local sig = '()Ljava/lang/String;'
     return jCall(andHelper, methodName, {}, sig)
 end
 
 ---@return string
-function native.getCocos2dxPackageName()
+function M.getCocos2dxPackageName()
     local methodName = 'getCocos2dxPackageName'
     local sig = '()Ljava/lang/String;'
     return jCall(andHelper, methodName, {}, sig)
@@ -39,14 +39,14 @@ end
 ---vibrate
 ---@param duration number @duration in seconds
 ---@return void
-function native.vibrate(duration)
+function M.vibrate(duration)
     local methodName = 'vibrate'
     local sig = '(F)V'
     return jCall(andHelper, methodName, { duration }, sig)
 end
 
 ---@return string
-function native.getVersion()
+function M.getVersion()
     local methodName = 'getVersion'
     local sig = '()Ljava/lang/String;'
     return jCall(andHelper, methodName, {}, sig)
@@ -54,7 +54,7 @@ end
 
 ---@param url string
 ---@return boolean
-function native.openURL(url)
+function M.openURL(url)
     local methodName = 'openURL'
     local sig = '(Ljava/lang/String;)Z'
     assert(type(url) == 'string')
@@ -62,14 +62,14 @@ function native.openURL(url)
 end
 
 ---@return number
-function native.getDPI()
+function M.getDPI()
     local methodName = 'getDPI'
     local sig = '()I'
     return jCall(andHelper, methodName, {}, sig)
 end
 
 ---@return string
-function native.getSDCardPath()
+function M.getSDCardPath()
     local methodName = 'getSDCardPath'
     local sig = '()Ljava/lang/String;'
     local ret = jCall(andApp, methodName, {}, sig)
@@ -79,9 +79,16 @@ function native.getSDCardPath()
     return ret
 end
 
+---@return string
+function M.messageBox(title, msg)
+    local methodName = 'showDialog'
+    local sig = '(Ljava/lang/String;Ljava/lang/String;)V'
+    return jCall(andHelper, methodName, { title, msg }, sig)
+end
+
 local ActivityInfo = require('platform.android.ActivityInfo')
 
-function native.setOrientation(ori)
+function M.setOrientation(ori)
     local methodName = 'setActivityOrientation'
     local sig = '(I)V'
     if type(ori) == 'string' then
@@ -92,23 +99,38 @@ function native.setOrientation(ori)
     return jCall(andApp, methodName, { ori }, sig)
 end
 
-function native.getOrientation()
+function M.setOrientationLandscape()
+    local glv = cc.Director:getInstance():getOpenGLView()
+    local sz = glv:getFrameSize()
+    M.setOrientation('SENSOR_LANDSCAPE')
+    glv:setFrameSize(math.max(sz.width, sz.height), math.min(sz.width, sz.height))
+end
+
+function M.setOrientationPortrait()
+    local glv = cc.Director:getInstance():getOpenGLView()
+    local sz = glv:getFrameSize()
+    M.setOrientation('PORTRAIT')
+    glv:setFrameSize(math.min(sz.width, sz.height), math.max(sz.width, sz.height))
+end
+
+---@return number
+function M.getOrientation()
     local methodName = 'getActivityOrientation'
     local sig = '()I'
     return jCall(andApp, methodName, {}, sig)
 end
 
 ---@return table
-function native.GetNativeInfo()
+function M.GetNativeInfo()
     local ret = {}
     if osname == 'android' then
-        ret['AssetsPath'] = native.getAssetsPath()
-        ret['PackageName'] = native.getCocos2dxPackageName()
-        ret['Version'] = native.getVersion()
-        ret['DPI'] = native.getDPI()
-        ret['SDCardPath'] = native.getSDCardPath()
+        ret['AssetsPath'] = M.getAssetsPath()
+        ret['PackageName'] = M.getCocos2dxPackageName()
+        ret['Version'] = M.getVersion()
+        ret['DPI'] = M.getDPI()
+        ret['SDCardPath'] = M.getSDCardPath()
     end
     return ret
 end
 
-return native
+return M
