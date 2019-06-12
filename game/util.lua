9b7c3a8a620c38a8cd8c9b957e1012b877033e5f
Xrysnow <xrysnow@outlook.com>
2019-06-12 14:33:52 +0800

    add collectResInfo to game/util.lua

--- game/util.lua
+++ game/util.lua
@@ -93,4 +93,67 @@ function M.getSlotStrings()
     end
 end
 
+local resTypeNames = {
+    [1] = 'Texture',
+    [2] = 'Sprite',
+    [3] = 'Animation',
+    [4] = 'Music',
+    [5] = 'SoundEffect',
+    [6] = 'Particle',
+    [7] = 'Font',
+    [8] = 'FX',
+    [9] = 'RenderTarget',
+}
+
+function M.collectResInfo()
+    local t = {}
+    local count = {
+        global       = 0,
+        stage        = 0,
+        Texture      = 0,
+        Sprite       = 0,
+        Animation    = 0,
+        Music        = 0,
+        SoundEffect  = 0,
+        Particle     = 0,
+        Font         = 0,
+        FX           = 0,
+        RenderTarget = 0,
+    }
+    local pools = lstg.getResourcePool()
+    for _, poolName in ipairs({ 'global', 'stage' }) do
+        local pool = pools[poolName]
+        for i = 1, #resTypeNames do
+            local p = pool[i]
+            local keys = p:keys()
+            table.sort(keys)
+            for i, key in ipairs(keys) do
+                ---@type lstg.Resource
+                local res = p:at(key)
+                local typeName = resTypeNames[res:getType()] or 'N/A'
+                local path = res:getPath()
+                local info = res:getInfo()
+                table.insert(t, {
+                    name     = key,
+                    info     = info,
+                    typeName = typeName,
+                    path     = path,
+                    poolName = poolName
+                })
+                if count[poolName] then
+                    count[poolName] = count[poolName] + 1
+                else
+                    count[poolName] = 1
+                end
+                if count[typeName] then
+                    count[typeName] = count[typeName] + 1
+                else
+                    count[typeName] = 1
+                end
+            end
+        end
+    end
+    return t, count
+end
+
 return M
