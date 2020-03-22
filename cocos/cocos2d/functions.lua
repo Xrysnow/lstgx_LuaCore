07d6f48b52e9072e3397e106f4c1f68a72d7e5c9
xrysnow <xrysnow@outlook.com>
2020-03-22 16:39:09 +0800

    Update functions.lua

--- cocos/cocos2d/functions.lua
+++ cocos/cocos2d/functions.lua
@@ -388,35 +388,28 @@ function handler(obj, method)
     end
 end
 
-function math.newrandomseed()
-    local ok, socket = pcall(function()
-        return require("socket")
-    end)
+--function math.newrandomseed()
+--    local ok, socket = pcall(function()
+--        return require("socket")
+--    end)
+--    if ok then
+--        math.randomseed(socket.gettime() * 1000)
+--    else
+--        math.randomseed(os.time())
+--    end
+--    math.random()
+--    math.random()
+--    math.random()
+--    math.random()
+--end
 
-    if ok then
-        math.randomseed(socket.gettime() * 1000)
-    else
-        math.randomseed(os.time())
-    end
-    math.random()
-    math.random()
-    math.random()
-    math.random()
-end
+--function math.round(value)
+--    value = checknumber(value)
+--    return math.floor(value + 0.5)
+--end
 
-function math.round(value)
-    value = checknumber(value)
-    return math.floor(value + 0.5)
-end
-
-local pi_div_180 = math.pi / 180
-function math.angle2radian(angle)
-    return angle * pi_div_180
-end
-
-function math.radian2angle(radian)
-    return radian * 180 / math.pi
-end
+--math.angle2radian = math.rad
+--math.radian2angle = math.deg
 
 function io.exists(path)
     local file = io.open(path, "r")
