919a4ab97e9e13135f33fc77bfa78130311a3bd2
Xrysnow <xrysnow@outlook.com>
2020-06-20 22:56:11 +0800

    update xe.logger

--- xe/logger.lua
+++ xe/logger.lua
@@ -3,11 +3,25 @@ local M = {}
 
 function M.log(str, type, trace)
     --print(type, str)
-    require('xe.main'):getInstance()._output:addLine(str, type)
+    str = tostring(str)
+    require('xe.main'):getInstance()._output:addLine(str, type, trace)
 end
 
 function M.clear()
     require('xe.main'):getInstance()._output:clear()
+end
+
+---@return fun
+function M.getLogger(trace, type)
+    if not type then
+        return function(type_, msg, ...)
+            M.log(msg:format(...), type_, trace)
+        end
+    else
+        return function(msg, ...)
+            M.log(msg:format(...), type, trace)
+        end
+    end
 end
 
 return M
