e0cbeadf245c0a8e06a41c717d566e3542ad6d58
Xrysnow <xrysnow@outlook.com>
2020-06-20 22:53:18 +0800

    refactor xe.input.String

--- xe/input/Path.lua
+++ xe/input/Path.lua
@@ -3,14 +3,26 @@ local base = require('xe.input.Base')
 local M = class('xe.input.String', base)
 local im = imgui
 local wi = require('imgui.Widget')
+local log = require('xe.logger').log
+local _res = {
+    loadsound      = true,
+    loadbgm        = true,
+    loadimage      = true,
+    loadtexture    = true,
+    loadimagegroup = true,
+    loadani        = true,
+    loadparticle   = true,
+    loadFX         = true, }
 
 ---@param node xe.SceneNode
 function M:ctor(node, idx, wildCard, isFolder)
     base.ctor(self, node, idx, 'path')
     local value = node:getAttrValue(idx) or ''
     self._value = value
+    local nt = node:getType()
 
     --TODO: use 'res://' if path in project dir
+    --TODO: copy file if not in project dir
     local btn = wi.Button(require('xe.ifont').FolderOpen, function()
         local dir = require('xe.Project').getDir()
         local path
@@ -20,10 +32,25 @@ function M:ctor(node, idx, wildCard, isFolder)
             path = require('platform.FileDialog').open(wildCard, dir)
         end
         if not path or path == '' then
-            require('xe.logger').log('File path not set.', 'warn', node:getType())
+            log('File path not set', 'warn', nt)
             return
         end
         self._value = path
+        if _res[nt] then
+            -- set res name to file name
+            local prop = require('xe.main').getProperty()
+            prop:setValue(2, string.filename(path))
+            if nt == 'loadparticle' then
+                local f, msg = io.open_u8(path, 'rb')
+                if f == nil then
+                    log(msg, "error", nt)
+                else
+                    local s = f:read(1)
+                    f:close()
+                    prop:setValue(3, 'parimg' .. (s:byte(1) + 1))
+                end
+            end
+        end
         self:submit()
     end)
 
@@ -32,7 +59,7 @@ function M:ctor(node, idx, wildCard, isFolder)
         local txt = self._value
         local popup
         if #txt > 30 then
-            txt = txt:sub(1, 27) .. '...'
+            txt = '...' .. txt:sub(-27, -1)
             popup = true
         end
         im.textWrapped(txt)
