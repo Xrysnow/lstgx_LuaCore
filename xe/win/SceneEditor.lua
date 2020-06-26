8145c8ef377c43d33c7460a6d7e92fde4941c516
Xrysnow <xrysnow@outlook.com>
2020-06-26 11:42:50 +0800

    update xe.SceneEditor

--- xe/win/SceneEditor.lua
+++ xe/win/SceneEditor.lua
@@ -1,9 +1,8 @@
 local base = require('imgui.widgets.Window')
----@class xe.ui.SceneManager:im.Window
-local M = class('xe.ui.SceneManager', base)
-local main = require('xe.main')
+---@class xe.SceneEditor:im.Window
+local M = class('xe.SceneEditor', base)
 local im = imgui
---local wi = require('imgui.Widget')
+local wi = require('imgui.Widget')
 
 function M:ctor()
     base.ctor(self, 'Scene Editor')
@@ -11,13 +10,41 @@ function M:ctor()
     --    im.setWindowFontScale(1.25)
     --end)
 
-    self._tree = require('xe.SceneTree')()
-    self._tree:addTo(self)
+    self._toolbar = require('xe.tools.ToolBar')()
+    self:addChild(self._toolbar)
 
+    self._toolpanel = require('xe.tools.ToolPanel')()
+    self:addChild(self._toolpanel)
+    self._toolpanel:disableAll()
+
+    local win = wi.ChildWindow('xe.scene.tree', im.vec2(-1, -1), true)
+    self:addChild(win)--:addChild(im.separator)
+    self._treewin = win
+
+    self._tree = require('xe.SceneTree')()
+    self._tree:addTo(win)
+
+    self._game = require('xe.win.GameView')()
+    self._game:addTo(self):setVisible(false)
 end
 
+---@return xe.SceneTree
 function M:getTree()
     return self._tree
+end
+
+function M:setEditor()
+    self._toolbar:setVisible(true)
+    self._toolpanel:setVisible(true)
+    self._treewin:setVisible(true)
+    self._game:setVisible(false)
+end
+
+function M:setGame()
+    self._toolbar:setVisible(false)
+    self._toolpanel:setVisible(false)
+    self._treewin:setVisible(false)
+    self._game:setVisible(true)
 end
 
 return M
