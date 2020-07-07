5f602f92aa1f776e3fef5d52d8ccfe8378a569cb
Xrysnow <xrysnow@outlook.com>
2020-07-07 16:22:27 +0800

    check input at submit

--- xe/input/Base.lua
+++ xe/input/Base.lua
@@ -35,12 +35,33 @@ function M:submit(value)
     self._node:setAttrEditType(self._idx, self._type)
     self._node:setAttrEditValue(self._idx, self._type, value)
     require('xe.SceneTree').submit()
+    self:_checkValid()
 end
 
 function M:getEditValue()
     local node = self._node
     local idx = self._idx
     return node:getAttrEditValue(idx, self._type) or node:getDefaultAttrValue(idx) or ''
+end
+
+function M:_checkValid()
+    local ok, msg = self._node:checkAttrEdit(self._idx)
+    if not ok then
+        if not msg or msg == '' then
+            msg = 'invalid'
+        end
+        self._error = msg
+    else
+        self._error = nil
+    end
+end
+
+function M:getError()
+    return self._error
+end
+
+function M:setError(v)
+    self._error = v
 end
 
 return M
