943f1eae44042bee56592f1c4132e424d7288da6
Xrysnow <xrysnow@outlook.com>
2020-07-15 11:42:43 +0800

    Update i18n.lua

--- i18n/i18n.lua
+++ i18n/i18n.lua
@@ -8,23 +8,28 @@ if cc.FileUtils:getInstance():isFileExist(f) then
     M._super = require(string.format('i18n.%s', code))
 end
 
-local function call(t, arg1)
+local function call(t, arg1, ...)
     local ty = type(arg1)
     if ty == 'string' then
-        return M.string(arg1)
+        return M.string(arg1, ...)
     elseif ty == 'table' then
+        local narg = select('#', ...)
         local ret = arg1[M._code]
         if ret == nil then
-            ret = arg1[1] or arg1.default or arg1.en
+            ret = arg1.default or arg1.en
         end
         local ty1 = type(ret)
         if ty1 == 'string' then
-            return ret
+            if narg > 0 then
+                return ret:format(...)
+            else
+                return ret
+            end
         elseif ty1 == 'function' then
-            return ret()
+            return ret(...)
         end
     elseif ty == 'function' then
-        return arg1(M._code)
+        return arg1(M._code, ...)
     elseif ty == 'nil' then
         return nil
     end
@@ -36,12 +41,17 @@ setmetatable(M, {
 
 ---@param str string
 ---@return string
-function M.string(str)
+function M.string(str, ...)
+    local s
     if M._super then
-        return M._super.string(str)
+        s = M._super.string(str)
     else
-        return str
+        s = str
     end
+    if select('#', ...) > 0 then
+        s = s:format(...)
+    end
+    return s
 end
 
 return M
