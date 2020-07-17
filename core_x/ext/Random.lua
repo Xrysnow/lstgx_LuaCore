c4a89371e736d4b865d16b069428dfcc4f9e044c
Xrysnow <xrysnow@outlook.com>
2020-07-17 12:26:04 +0800

    add Random:sample_weighted()

--- core_x/ext/Random.lua
+++ core_x/ext/Random.lua
@@ -4,6 +4,8 @@
 local M = lstg.Random
 local insert = table.insert
 local floor = math.floor
+local pow = math.pow
+local log = math.log
 
 --- Choose a random element from a non-empty array.
 ---@param array table
@@ -74,6 +76,73 @@ function M:sample_range(start, stop, step, k)
         local j = self:below(i) + 1
         insert(result, start + step * (mark[j] or j - 1))
         mark[j] = mark[k - i + 1] or n - i + 1
+    end
+    return result
+end
+
+--- Chooses k unique random elements from an array by given weight.
+---
+--- Reference: https://www.sciencedirect.com/science/article/abs/pii/S002001900500298X
+---
+--- Returns an array.
+---@param array table
+---@param weight table
+---@param k number
+---@return table
+function M:sample_weighted(array, weight, k)
+    local n = #array
+    if k > n or n == 0 or k < 1 then
+        return {}
+    end
+    local heap = {}
+    local Xw
+    local Tw, w_acc = 0, 0
+    for i, sample in ipairs(array) do
+        if #heap < k then
+            local wi = weight[i] or 0
+            local ui = self:uniform(0, 1)
+            local ki = pow(ui, 1 / wi)
+            insert(heap, { ki, sample })
+            if #heap == k then
+                table.sort(heap, function(a, b)
+                    return a[1] > b[1]
+                end)
+            end
+            -- continue
+        else
+            if w_acc == 0 then
+                Tw = heap[#heap][1]
+                local r = self:uniform(0, 1)
+                Xw = log(r) / log(Tw)
+            end
+            local wi = weight[i] or 0
+            if w_acc + wi < Xw then
+                w_acc = w_acc + wi
+                -- continue
+            else
+                w_acc = 0
+                local tw = pow(Tw, wi)
+                local r2 = self:uniform(tw, 1)
+                local ki = pow(r2, 1 / wi)
+                heap[#heap] = nil
+                if #heap == 0 or ki > heap[1][1] then
+                    insert(heap, 1, { ki, sample })
+                elseif ki <= heap[#heap][1] then
+                    insert(heap, { ki, sample })
+                else
+                    for j = 1, k - 1 do
+                        if heap[j][1] >= ki and ki > heap[j + 1][1] then
+                            insert(heap, j, { ki, sample })
+                            break
+                        end
+                    end
+                end
+            end
+        end
+    end
+    local result = {}
+    for _, v in ipairs(heap) do
+        insert(result, v[2])
     end
     return result
 end
