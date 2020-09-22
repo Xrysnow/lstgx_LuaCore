197ef5d54171f0f7b99c8a8c01ac7d59a9209e53
Xrysnow <xrysnow@outlook.com>
2020-09-22 22:35:49 +0800

    refactor xclass

--- core_x/util/xclass.lua
+++ core_x/util/xclass.lua
@@ -4,24 +4,49 @@
 --- Copyright (C) 2018-2020 Xrysnow. All rights reserved.
 ---
 
+local type = type
+local pairs = pairs
+local RawNew = lstg.RawNew
 
-local New = lstg.New
+local callbacks = {
+    init   = true,
+    del    = true,
+    frame  = true,
+    render = true,
+    colli  = true,
+    kill   = true,
+}
 
----Extends [Class] function so that you can use ClassName(...) to create an instance of lstg object.
----Example: `classname = xclass(object)`
+--- Create extended game object class.
+--- You can use classname(...) to create an instance of game object.
+--- Example: `classname = xclass(object)`
 ---@see object
 ---@param base object
 ---@param define table
 ---@return object
-function xclass(base, define)
-    local ret = Class(base, define)
+function xclass(base)
+    local ret = Class(base, base)
     ret['.x'] = true
     if base and base['.3d'] then
         ret['.3d'] = true
     end
+    local methods
+    local function get_methods()
+        for k, v in pairs(ret) do
+            if type(v) == 'function' and type(k) == 'string' and not callbacks[k] then
+                methods[k] = v
+            end
+        end
+    end
     local mt = { __call = function(t, ...)
-        local obj = New(ret, ...)
-        table.supplement(obj, ret)
+        local obj = RawNew(ret)
+        if not methods then
+            get_methods()
+        end
+        for k, v in pairs(methods) do
+            obj[k] = v
+        end
+        ret[1](obj, ...)
         return obj
     end }
     return setmetatable(ret, mt)
